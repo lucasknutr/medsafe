60dcbefbb76e981cd6e5e8648201fe8a7f872934
Lucas <lucasclaudinocc@gmail.com>
2025-05-02 00:47:07 -0300

    Turning Supabase endpoints into Prisma

--- src/app/pagamento/page.tsx
+++ src/app/pagamento/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 export const dynamic = "force-dynamic";
 import PaymentForm from "@/app/components/PaymentForm";
+import { Suspense } from "react";
 
 export default function PagamentoPage() {
   return (
     <div className="min-h-screen py-8">
-      <PaymentForm />
+      <Suspense fallback={<div>Carregando pagamento...</div>}>
+        <PaymentForm />
+      </Suspense>
     </div>
   );
 }
