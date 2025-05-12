8ea63819c8d20221e62ea7ac75209c98f286de86
Lucas <lucasclaudinocc@gmail.com>
2025-05-11 22:42:54 -0300

    Fixing lint errors

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import React from 'react';
 import RegisterForm from '@/app/components/RegisterForm';
 import Navbar from '@/app/components/Navbar'; 
 
 export default function RegisterPage() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* Adjusted to pt-40 for navbar, pb-8 for bottom padding */}
       <div className="min-h-screen pt-40 pb-8">
         <RegisterForm />
